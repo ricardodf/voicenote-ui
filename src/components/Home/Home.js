111177ee4ed36cb712d1ddd8ec0acd30787f8f6a
Ricardo Diaz <39528804+ricardodf@users.noreply.github.com>
2021-05-01 15:59:57 -0500

    Landing Page

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,11 +1,12 @@
 import React from 'react'
+import './Home.css'
 
 class Home extends React.Component {
   render () {
     return (
-      <>
+      <div className="hm__container">
         <h1>This is Home</h1>
-      </>
+      </div>
     )
   }
 }
--- src/components/LandingPage/LandingPage.js
+++ src/components/LandingPage/LandingPage.js
@@ -1,11 +1,41 @@
 import React from 'react'
-import styles from './LandingPage.css'
+import { Button, Navbar } from 'react-bootstrap'
+import { LP } from './LandingPageConsts'
+import './LandingPage.css'
 
 class LandingPage extends React.Component {
   render () {
     return (
       <div className="lp__container">
-        <h1>This is LandingPage</h1>
+        <Navbar className="lp__nav" expand="lg">
+          <div className="lp__nav-main">
+            <img
+              className="lp__nav-img"
+              src={'/images/navbar-icon.png'}
+              alt=""
+            />
+            <Navbar.Brand className="lp__nav-title">
+              {LP.NAV_TITLE}
+            </Navbar.Brand>
+          </div>
+        </Navbar>
+        <div className="lp__main">
+          <div className="lp__left">
+            <div className="lp__text">{LP.LEFT_TEXT}</div>
+            <div className="lp__btn-group">
+              <Button className="lp__btn btn-account">{LP.NEW_ACCOUNT}</Button>
+              <hr className="lp__division" />
+              <Button className="lp__btn btn-login">{LP.LOG_IN}</Button>
+            </div>
+          </div>
+          <div className="lp__right">
+            <img
+              className="lp__bg-svg"
+              src="/images/topography.svg"
+              alt=""
+            ></img>
+          </div>
+        </div>
       </div>
     )
   }
